902b87b1e6ff3ef828324d0691c1c3882358f868
Abhinav2008-lab <75878501+Abhinav2008-lab@users.noreply.github.com>
2021-08-03 07:14:45 -0700

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,42 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    question.hide();
+    background("white");
+    textSize(30);
+    fill("red");
+    text("YOU WILL RECIVE 100 FOLLOWERS IN 1 HOUR",340,50);
+    //text("..........................",320,65);
+    Contestant.getPlayerInfo();
+
+  }
+
+}
